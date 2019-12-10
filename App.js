2960c05bfe402e9402cdf1c26a2ce1840123cb3f
ddock4you <ddock4you@gmail.com>
2019-12-10 20:09:24 +0900

    - expo 구동 테스트

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.yellowView}>hello?</Text>
+      <Text style={styles.blueView}>okay hello</Text>
     </View>
   );
 }
@@ -13,7 +14,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    
   },
+  yellowView: {
+    flex: 1,
+    backgroundColor: 'yellow',
+  },
+  blueView: {
+    flex: 1,
+    backgroundColor: 'blue',
+  }
 });
