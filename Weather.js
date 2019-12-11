ea6ff487c19748d25fac36d785cbdec5a4993101
ddock4you <ddock4you@gmail.com>
2019-12-11 17:11:22 +0900

    - 날씨별로 텍스트 내용 추가, css 약간 수정, 완성

--- Weather.js
+++ Weather.js
@@ -5,52 +5,65 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
 
 const weatherOptions = {
-    Rain: {
-      iconName: "weather-hail",
-      gradient: ["#4DA0B0", "#D39D38"]
-    },
     Thunderstorm: {
-      iconName: "",
-      gradient: []
+      iconName: "weather-lightning",
+      gradient: ["#373B44", "#4286f4"],
+      title: "Thunderstorm in the house",
+      subtitle: "Actually, outside of the house"
     },
     Drizzle: {
-      iconName: "",
-      gradient: []
+      iconName: "weather-hail",
+      gradient: ["#89F7FE", "#66A6FF"],
+      title: "Drizzle",
+      subtitle: "Is like rain, but gay 🏳️‍🌈"
     },
-    Haze: {
-      iconName: "",
-      gradient: []
+    Rain: {
+      iconName: "weather-rainy",
+      gradient: ["#00C6FB", "#005BEA"],
+      title: "Raining like a MF",
+      subtitle: "For more info look outside"
     },
     Snow: {
-      iconName: "",
-      gradient: []
+      iconName: "weather-snowy",
+      gradient: ["#7DE2FC", "#B9B6E5"],
+      title: "Cold as balls",
+      subtitle: "Do you want to build a snowman? Fuck no."
     },
     Atmosphere: {
-      iconName: "",
-      gradient: []
+      iconName: "weather-hail",
+      gradient: ["#89F7FE", "#66A6FF"]
     },
     Clear: {
-      iconName: "",
-      gradient: []
+      iconName: "weather-sunny",
+      gradient: ["#FF7300", "#FEF253"],
+      title: "Sunny as fuck",
+      subtitle: "Go get your ass burnt"
     },
     Clouds: {
-      iconName: "",
-      gradient: []
-    },
-    Haze: {
-      iconName: "",
-      gradient: []
+      iconName: "weather-cloudy",
+      gradient: ["#D7D2CC", "#304352"],
+      title: "Clouds",
+      subtitle: "I know, fucking boring"
     },
     Mist: {
-      iconName: "",
-      gradient: []
+      iconName: "weather-hail",
+      gradient: ["#4DA0B0", "#D39D38"],
+      title: "Mist!",
+      subtitle: "It's like you have no glasses on."
     },
     Dust: {
-      iconName: "",
-      gradient: []
+      iconName: "weather-hail",
+      gradient: ["#4DA0B0", "#D39D38"],
+      title: "Dusty",
+      subtitle: "Thanks a lot China 🖕🏻"
+    },
+    Haze: {
+      iconName: "weather-hail",
+      gradient: ["#4DA0B0", "#D39D38"],
+      title: "Haze",
+      subtitle: "Just don't go outside."
     }
   };
-  
 
 export default function Weather({temp, condition}) {
     return (
@@ -60,10 +73,15 @@ export default function Weather({temp, condition}) {
         >
             <StatusBar barStyle="light-content" />
             <View style={styles.halfContainer}>
-                <MaterialCommunityIcons size={96} name="weather-lightning-rainy" />
-                <Text style={styles.temp}>{temp}o</Text>
+                <MaterialCommunityIcons size={96} name={weatherOptions[condition].iconName} color='white' />
+                <Text style={styles.temp}>{temp}°C</Text>
             </View>
-            <View style={styles.halfContainer} />
+            <View style={styles.textContainer}>
+                <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                <Text style={styles.subtitle}>
+                {weatherOptions[condition].subtitle}
+                </Text>
+            </View>
         </LinearGradient>
     )
 };
@@ -92,11 +110,30 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   temp: {
-    fontSize: 42
+    fontSize: 42,
+    color: 'white'
   },
   halfContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  title: {
+    color: "white",
+    fontSize: 44,
+    fontWeight: "300",
+    marginBottom: 10,
+    textAlign: 'left',
+  },
+  subtitle: {
+    fontWeight: "600",
+    color: "white",
+    fontSize: 24
+  },
+  textContainer: {
+    alignItems: "flex-start",
+    paddingHorizontal: 40,
+    justifyContent: "center",
+    flex: 1
   }
 });
